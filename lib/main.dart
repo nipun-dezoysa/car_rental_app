0ebcd450bcb1f0b3390518e623a6e13f5153b2e4
nipun-dezoysa <nipunavishka123@gmail.com>
2025-02-08 19:05:04 +0530

    feat: add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,35 @@
 import 'package:car_rental_app/firebase_options.dart';
-import 'package:car_rental_app/presentation/pages/car_details_page.dart';
-import 'package:car_rental_app/presentation/pages/car_list_screen.dart';
-import 'package:car_rental_app/presentation/pages/maps_details_page.dart';
+import 'package:car_rental_app/injection_container.dart';
+import 'package:car_rental_app/presentation/bloc/car_bloc.dart';
+import 'package:car_rental_app/presentation/bloc/car_event.dart';
 import 'package:car_rental_app/presentation/pages/onboarding_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  initInjection();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (_)=>getIt<CarBloc>()..add(LoadCars()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: OnboardingPage(),
       ),
-      home: OnboardingPage(),
     );
   }
 }
--- lib/presentation/pages/car_list_screen.dart
+++ lib/presentation/pages/car_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:car_rental_app/data/models/Car.dart';
+import 'package:car_rental_app/presentation/bloc/car_bloc.dart';
+import 'package:car_rental_app/presentation/bloc/car_state.dart';
 import 'package:car_rental_app/presentation/widgets/car_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CarListScreen extends StatelessWidget {
   CarListScreen({super.key});
@@ -27,17 +30,26 @@ class CarListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Choose Your Car'),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-      ),
-      body: ListView.builder(
-        itemCount: cars.length,
-        itemBuilder: (context, index) {
-          return CarCard(car: cars[index]);
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Choose Your Car'),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+        body: BlocBuilder<CarBloc, CarState>(builder: (context, state) {
+          if (state is CarsLoading) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is CarsLoaded) {
+            return ListView.builder(
+              itemCount: state.cars.length,
+              itemBuilder: (context, index) {
+                return CarCard(car: state.cars[index]);
+              },
+            );
+          } else if (state is CarsError) {
+            return Center(child: Text('error : ${state.message}'));
+          } else {
+            return Center(child: Text('error'));
+          }
+        }));
   }
 }
